39b2c2eb0c12526b643e782103626dd53deb2676
BeggaJoe <86000868+BeggaJoe@users.noreply.github.com>
2022-01-23 23:13:12 +0100

    Update BasicModel.py

--- BasicModel.py
+++ BasicModel.py
@@ -7,8 +7,8 @@ class Delivery:
         self.Gebietsdaten = pd.read_csv(r'C:\Users\Juli\Desktop\Jochens Stuff\Uni\Master\WS 20-21\Masterarbeit\Masterarbeit\Bearbeitung\Datengrundlage\Kundenstamm_im_Einzugsgebiet.csv',
                                         delimiter=";", encoding='iso-8859-15')
 
-        self.Ortschaften_Index = {Gebietsdaten["Ortschaft"][i]: (Gebietsdaten["Kategorie"][i], Gebietsdaten["OSM_ID"][i])
-                                  for i in range(len(Gebietsdaten))}
+        self.Ortschaften_Index = {self.Gebietsdaten["Ortschaft"][i]: (self.Gebietsdaten["Kategorie"][i], self.Gebietsdaten["OSM_ID"][i])
+                                  for i in range(len(self.Gebietsdaten))}
        
     def give(self, Zahl = 5):
         self.Gebietsdaten[0:Zahl]
